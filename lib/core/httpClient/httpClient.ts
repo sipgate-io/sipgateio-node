ae421b07d7e4d14c372619837b823f9e401d81b5
Karim Echchennouf <echchennouf@sipgate.de>
2019-08-12 10:58:08 +0200

    feed: add axios wrapper

---
+++ lib/core/httpClient/httpClient.ts
@@ -0,0 +1,51 @@
+import axios, { AxiosInstance, AxiosRequestConfig, AxiosResponse } from 'axios';
+import btoa from 'btoa';
+export const BASE_URL = 'https://api.sipgate.com/v2';
+export class HttpClient {
+  private client: AxiosInstance;
+
+  constructor(username: string, password: string, baseUrl?: string) {
+    const basicAuth = btoa(`${username}:${password}`);
+    this.client = axios.create({
+      baseURL: baseUrl || BASE_URL,
+      headers: {
+        Authorization: `Basic ${basicAuth}`,
+      },
+    });
+  }
+
+  public get(url: string, config?: AxiosRequestConfig): Promise<AxiosResponse> {
+    return this.client.get(url, config);
+  }
+
+  public post(
+    url: string,
+    data?: any,
+    config?: AxiosRequestConfig,
+  ): Promise<AxiosResponse> {
+    return this.client.post(url, data, config);
+  }
+
+  public put(
+    url: string,
+    data?: any,
+    config?: AxiosRequestConfig,
+  ): Promise<AxiosResponse> {
+    return this.client.put(url, data, config);
+  }
+
+  public delete(
+    url: string,
+    config?: AxiosRequestConfig,
+  ): Promise<AxiosResponse> {
+    return this.client.delete(url, config);
+  }
+
+  public patch(
+    url: string,
+    data?: any,
+    config?: AxiosRequestConfig,
+  ): Promise<AxiosResponse> {
+    return this.client.put(url, data, config);
+  }
+}
