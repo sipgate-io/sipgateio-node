78af3e8277dab4a53a7809e27c5620d519901417
Tim Tressel <tressel@sipgate.de>
2019-08-12 16:05:56 +0200

    wip: test - add header test

--- lib/core/httpClient/test/httpClient.test.ts
+++ lib/core/httpClient/test/httpClient.test.ts
@@ -1,25 +1,46 @@
-import axios from 'axios';
-import axiosMock from 'axios-mock-adapter';
+import axios, { AxiosInstance } from 'axios';
+import MockAdapter from 'axios-mock-adapter';
+import btoa from 'btoa';
 import { BASE_URL, HttpClient } from '../httpClient';
 
-const mock = new axiosMock(axios);
-
 describe('Test Get Method', () => {
+  const mock = new MockAdapter(axios);
+  const TESTDATA = 'TESTDATA';
+
   test('Test Get to Get Mapping', async () => {
     const httpClient = new HttpClient('testUsername', 'testPassword');
 
-    mock.onGet('').reply(200);
+    mock.onGet('').reply(200, TESTDATA);
 
     const response = await httpClient.get('');
-    expect(response.status).toBe(200);
+    expect(response.data).toBe(TESTDATA);
   });
 
   test('Test Valid URL Concatenation', async () => {
     const httpClient = new HttpClient('testUsername', 'testPassword');
-
-    mock.onGet(`${BASE_URL}/sessions`).reply(200);
+    mock.onGet(`${BASE_URL}/sessions`).reply(200, TESTDATA);
 
     const response = await httpClient.get('/sessions');
-    expect(response.status).toBe(200);
+    expect(response.data).toBe(TESTDATA);
+  });
+
+  test('test headers', async () => {
+    const httpClient = new HttpClient('testUsername', 'testPassword');
+    const client = Reflect.get(httpClient, 'client') as AxiosInstance;
+    const headers = client.defaults.headers;
+
+    const expectedXHeaderKey = 'X-Sipgate-Client';
+    const expectedXHeaderValue = 'lib-node';
+
+    const expectedXVersionHeaderKey = 'X-Sipgate-Version';
+    const expectedXVersionHeaderValue = '0.0.0';
+
+    const expectedAuthHeader = `Basic ${btoa('testUsername:testPassword')}`;
+
+    expect(headers.Authorization).toEqual(expectedAuthHeader);
+    expect(headers[expectedXHeaderKey]).toEqual(expectedXHeaderValue);
+    expect(headers[expectedXVersionHeaderKey]).toEqual(
+      expectedXVersionHeaderValue,
+    );
   });
 });
