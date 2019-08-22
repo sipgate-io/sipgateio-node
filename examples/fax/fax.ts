d5dc286fcda8a8cfdf5fb6b3278172bb6681c975
Sven Kube <kube@sipgate.de>
2019-08-22 11:21:27 +0200

    docs: add sms and fax example

---
+++ examples/fax/fax.ts
@@ -0,0 +1,22 @@
+// tslint:disable no-console
+import { Fax } from '../../lib/core/models';
+import { createClient } from '../../lib/core/sipgateClient';
+
+const username = process.env.SIPGATE_USERNAME || '';
+const password = process.env.SIPGATE_PASSWORD || '';
+
+const client = createClient(username, password);
+
+const fax: Fax = {
+  filename: './testpage.pdf',
+  recipient: process.env.SIPGATE_FAX_RECIPIENT || '',
+};
+
+client.fax
+  .send(fax)
+  .then(() => {
+    console.log('Fax sent');
+  })
+  .catch(error => {
+    console.error(error);
+  });
