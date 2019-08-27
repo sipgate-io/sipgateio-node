f51a21c45f8fedf3203d206cc97923d2f815ebf5
Hendryk Weiland <Hendryk.Weiland@hhu.de>
2019-08-27 16:59:53 +0200

    chore: add network error

--- lib/sms/sms.ts
+++ lib/sms/sms.ts
@@ -1,3 +1,4 @@
+import { ConnectionError } from '../core/errors/ConnectionError';
 import { ExtensionError } from '../core/errors/ExtensionError';
 import handleCoreError from '../core/errors/handleCoreError';
 import { HttpClientModule } from '../core/httpClient/httpClient.module';
@@ -18,11 +19,16 @@ export const createSMSModule = (client: HttpClientModule): SMSModule => ({
   },
 });
 
-// Todo handle error if the server isn't reachable
 const handleError = (e: any) => {
+  if (
+    e.message === 'Network Error' ||
+    e.message.includes('getaddrinfo ENOTFOUND')
+  ) {
+    return new ConnectionError();
+  }
   if (e.response.status === 403) {
     return new ExtensionError('Invalid SMS extension');
   }
 
-  return handleCoreError(e);
+  return handleCoreError(e.message);
 };
