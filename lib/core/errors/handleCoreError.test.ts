220310dfd3f5d26331ff65acaed45962f6fa9fb6
Hendryk Weiland <Hendryk.Weiland@hhu.de>
2019-08-27 14:35:14 +0200

    test: complete tests for core errors

--- lib/core/errors/handleCoreError.test.ts
+++ lib/core/errors/handleCoreError.test.ts
@@ -7,6 +7,13 @@ describe('handleCoreError', () => {
     expect(handleCoreError(error)).toEqual(new AuthenticationError());
   });
 
+  test('AccessError', () => {
+    const error = { response: { status: 403 } };
+    expect(handleCoreError(error)).toEqual(
+      new AuthenticationError('Forbidden'),
+    );
+  });
+
   test('Catch all errors', () => {
     const error = { response: { status: 3.1415 } };
     expect(handleCoreError(error)).toEqual(new Error());
