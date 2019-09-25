94d003d19f4d918a3eea53824a63b99ea4ea2734
Jan Schmitt <schmitt@sipgate.de>
2019-09-25 14:37:04 +0200

    fix: fax example having fax object as param. added fax-example npm script.

--- examples/fax/fax.ts
+++ examples/fax/fax.ts
@@ -8,14 +8,14 @@ const username = process.env.SIPGATE_USERNAME || '';
 
 const client = createClient(username, password);
 
-const faxlineID = process.env.SIPGATE_FAX_EXTENSION || '';
+const faxlineId = process.env.SIPGATE_FAX_EXTENSION || '';
 const recipient = process.env.SIPGATE_FAX_RECIPIENT || '';
 const filePath = './testpage.pdf';
 const { name: filename } = path.parse(path.basename(filePath));
 const fileContent = fs.readFileSync(filePath);
 
 client.fax
-  .send(recipient, fileContent, filename, faxlineID)
+  .send({ recipient, fileContent, filename, faxlineId })
   .then(() => {
     console.log('Fax sent');
   })
