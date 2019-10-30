fca60a1c3fc13f5570efa3b4dac1bd9858da7548
Jan Schmitt <schmitt@sipgate.de>
2019-10-30 14:52:50 +0100

    wip: add endpoint url string constant. import handleError in settings module.

--- lib/settings/settings.ts
+++ lib/settings/settings.ts
@@ -1,21 +1,37 @@
+import { ConnectionError, ErrorMessage } from '../core/errors';
 import { HttpClientModule } from '../core/httpClient';
 import { Settings } from '../core/models';
 import { SettingsModule } from './settings.module';
+import handleCoreError from '../core/errors/handleCoreError';
+
+const SETTINGS_ENDPOINT = 'settings/sipgateio';
 
 export const createSettingsModule = (
 	client: HttpClientModule
 ): SettingsModule => ({
 	async setIncomingUrl(url: string): Promise<void> {
-		await getSettings(client).then(settings => {
-			settings.incomingUrl = url;
-			client.put(url, settings);
-		});
+		await getSettings(client)
+			.then(settings => {
+				settings.incomingUrl = url;
+				client.put(SETTINGS_ENDPOINT, settings);
+			})
+			.catch(error => handleError(error));
 	}
 });
 
 const getSettings = async (client: HttpClientModule): Promise<Settings> => {
 	return client
-		.get('settings/sipgateio')
+		.get(SETTINGS_ENDPOINT)
 		.then(res => res.data)
-		.catch(error => error);
+		.catch(error => handleError(error));
+};
+
+const handleError = (e: any) => {
+	if (
+		e.message === 'Network Error' ||
+		e.message.includes(ErrorMessage.NETWORK_ERROR)
+	) {
+		return new ConnectionError();
+	}
+	return handleCoreError(e.message);
 };
