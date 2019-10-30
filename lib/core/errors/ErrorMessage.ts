eaa6a87580a9f55d603a6e515dbb9d77c9c2d02c
Raul Lugo <lugo@sipgate.de>
2019-10-30 16:32:28 +0100

    test: webhook URL validator

--- lib/core/errors/ErrorMessage.ts
+++ lib/core/errors/ErrorMessage.ts
@@ -4,6 +4,7 @@ export enum ErrorMessage {
 	VALIDATOR_INVALID_PASSWORD = 'Invalid password',
 	VALIDATOR_INVALID_PHONE_NUMBER = 'Invalid Phone Number',
 	VALIDATOR_INVALID_PDF_MIME_TYPE = 'Invalid PDF File',
+	VALIDATOR_INVALID_WEBHOOK_URL = 'Invalid Webhook URL',
 
 	SMS_INVALID_MESSAGE = 'Invalid SMS message',
 	SMS_INVALID_EXTENSION = 'Invalid SMS extension',
---
+++ lib/core/validator/validateWebhookUrl.test.ts
@@ -0,0 +1,21 @@
+import { ErrorMessage } from '../errors';
+import { validateWebhookUrl } from './validateWebhookUrl';
+
+describe('ValidateWebhookUrl', () => {
+	test.each`
+		input                  | expected
+		${'http://'}           | ${{ isValid: true }}
+		${'https://'}          | ${{ isValid: true }}
+		${'https://valid.com'} | ${{ isValid: true }}
+		${'httptest://'}       | ${{ isValid: false, cause: ErrorMessage.VALIDATOR_INVALID_WEBHOOK_URL }}
+		${''}                  | ${{ isValid: false, cause: ErrorMessage.VALIDATOR_INVALID_WEBHOOK_URL }}
+		${'httpsx'}            | ${{ isValid: false, cause: ErrorMessage.VALIDATOR_INVALID_WEBHOOK_URL }}
+		${'://'}               | ${{ isValid: false, cause: ErrorMessage.VALIDATOR_INVALID_WEBHOOK_URL }}
+		${'www.url.com'}       | ${{ isValid: false, cause: ErrorMessage.VALIDATOR_INVALID_WEBHOOK_URL }}
+	`(
+		'validator returns $expected when $input is validated',
+		({ input, expected }) => {
+			expect(validateWebhookUrl(input)).toEqual(expected);
+		}
+	);
+});
