be39432052835eac7d6815cf97e386234594dbce
Raul Lugo <lugo@sipgate.de>
2019-10-30 16:33:38 +0100

    feat: webhook URL validator

---
+++ lib/core/validator/validateWebhookUrl.ts
@@ -0,0 +1,15 @@
+import { ErrorMessage } from '../errors';
+import { ValidationResult } from './validationResult';
+
+const validateWebhookUrl = (url: string): ValidationResult => {
+	const webhookUrlRegex = new RegExp(/^(http|https):\/\//i);
+
+	if (!webhookUrlRegex.test(url)) {
+		return {
+			cause: ErrorMessage.VALIDATOR_INVALID_WEBHOOK_URL,
+			isValid: false
+		};
+	}
+	return { isValid: true };
+};
+export { validateWebhookUrl };
