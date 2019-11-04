5d76e11c71fe03070f717486e843efdc17fca73d
Sven Kube <kube@sipgate.de>
2019-11-04 15:11:12 +0100

    test: add test for setIncomingUrl and setOutgoingUrl

--- lib/settings/settings.test.ts
+++ lib/settings/settings.test.ts
@@ -1,4 +1,6 @@
+import { ErrorMessage } from '../core/errors';
 import { HttpClientModule } from '../core/httpClient';
+import { Settings } from '../core/models';
 import { SettingsModule } from './settings.module';
 import { createSettingsModule } from './settings';
 
@@ -27,5 +29,103 @@ describe('get settings', () => {
 		await mockedSettingsModule.setIncomingUrl('https://test.de');
 
 		expect(mockClient.get).toBeCalledWith('settings/sipgateio');
+	});
+});
+
+describe('setIncomingUrl', () => {
+	let mockClient: HttpClientModule;
+
+	const TEST_INCOMING_URL = 'http://newIncoming.url';
+
+	beforeAll(() => {
+		mockClient = {} as HttpClientModule;
+	});
+
+	it('should set supplied incomingUrl in fetched settings object', async () => {
+		const settingsModule = createSettingsModule(mockClient);
+
+		const settings: Settings = {
+			incomingUrl: '',
+			log: false,
+			outgoingUrl: '',
+			whitelist: []
+		};
+
+		const expectedSettings = { ...settings };
+		expectedSettings.incomingUrl = TEST_INCOMING_URL;
+
+		mockClient.get = jest
+			.fn()
+			.mockImplementationOnce(() =>
+				Promise.resolve({ status: 200, data: settings })
+			);
+
+		mockClient.put = jest
+			.fn()
+			.mockImplementationOnce(() => Promise.resolve({ status: 200, data: {} }));
+
+		await settingsModule.setIncomingUrl(TEST_INCOMING_URL);
+
+		await expect(mockClient.put).toBeCalledWith(
+			expect.anything(),
+			expectedSettings
+		);
+	});
+
+	it('should throw an error when supplied with an invalid url', async () => {
+		const settingsModule = createSettingsModule(mockClient);
+
+		await expect(settingsModule.setIncomingUrl('newUrl')).rejects.toThrowError(
+			new Error(ErrorMessage.VALIDATOR_INVALID_WEBHOOK_URL)
+		);
+	});
+});
+
+describe('setOutgoingUrl', () => {
+	let mockClient: HttpClientModule;
+
+	const TEST_OUTGOING_URL = 'http://newOutgoing.url';
+
+	beforeAll(() => {
+		mockClient = {} as HttpClientModule;
+	});
+
+	it('should set supplied incomingUrl in fetched settings object', async () => {
+		const settingsModule = createSettingsModule(mockClient);
+
+		const settings: Settings = {
+			incomingUrl: '',
+			log: false,
+			outgoingUrl: '',
+			whitelist: []
+		};
+
+		const expectedSettings = { ...settings };
+		expectedSettings.outgoingUrl = TEST_OUTGOING_URL;
+
+		mockClient.get = jest
+			.fn()
+			.mockImplementationOnce(() =>
+				Promise.resolve({ status: 200, data: settings })
+			);
+
+		mockClient.put = jest
+			.fn()
+			.mockImplementationOnce(() => Promise.resolve({ status: 200, data: {} }));
+
+		await settingsModule.setOutgoingUrl(TEST_OUTGOING_URL);
+
+		await expect(mockClient.put).toBeCalledWith(
+			expect.anything(),
+			expectedSettings
+		);
+	});
+
+	it('should throw an error when supplied with an invalid url', async () => {
+		const settingsModule = createSettingsModule(mockClient);
+
+		await expect(settingsModule.setOutgoingUrl('newUrl')).rejects.toThrowError(
+			new Error(ErrorMessage.VALIDATOR_INVALID_WEBHOOK_URL)
+		);
 	});
 });
