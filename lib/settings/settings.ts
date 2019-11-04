df9803741eb12800a7e0d23daf6ed3e4e4ca569b
Sven Kube <kube@sipgate.de>
2019-11-04 10:54:53 +0100

    refactor: implement modifySettings whith closure to reduce code duplication

--- lib/settings/settings.ts
+++ lib/settings/settings.ts
@@ -10,52 +10,22 @@ export const createSettingsModule = (
 	client: HttpClientModule
 ): SettingsModule => ({
 	async setIncomingUrl(url): Promise<void> {
-		await getSettings(client)
-			.then(settings => {
-				settings.incomingUrl = url;
-				client.put(SETTINGS_ENDPOINT, settings);
-			})
-			.catch(error => handleError(error));
+		modifySettings(client, settings => (settings.incomingUrl = url));
 	},
 	async setOutgoingUrl(url): Promise<void> {
-		await getSettings(client)
-			.then(settings => {
-				settings.outgoingUrl = url;
-				client.put(SETTINGS_ENDPOINT, settings);
-			})
-			.catch(error => handleError(error));
+		modifySettings(client, settings => (settings.outgoingUrl = url));
 	},
 	async setWhitelist(extensions): Promise<void> {
-		await getSettings(client)
-			.then(settings => {
-				settings.whitelist = extensions;
-				client.put(SETTINGS_ENDPOINT, settings);
-			})
-			.catch(error => handleError(error));
+		modifySettings(client, settings => (settings.whitelist = extensions));
 	},
 	async setLog(value): Promise<void> {
-		await getSettings(client)
-			.then(settings => {
-				settings.log = value;
-				client.put(SETTINGS_ENDPOINT, settings);
-			})
-			.catch(error => handleError(error));
+		modifySettings(client, settings => (settings.log = value));
 	},
 	async clearIncomingUrl(): Promise<void> {
-		await getSettings(client)
-			.then(settings => {
-				settings.incomingUrl = '';
-				client.put(SETTINGS_ENDPOINT, settings);
-			})
-			.catch(error => handleError(error));
+		modifySettings(client, settings => (settings.incomingUrl = ''));
 	},
 	async clearOutgoingUrl(): Promise<void> {
-		await getSettings(client)
-			.then(settings => {
-				settings.outgoingUrl = '';
-				client.put(SETTINGS_ENDPOINT, settings);
-			})
-			.catch(error => handleError(error));
+		modifySettings(client, settings => (settings.outgoingUrl = ''));
 	}
 });
 
@@ -63,6 +33,15 @@ const getSettings = async (client: HttpClientModule): Promise<Settings> => {
 	return client
 		.get(SETTINGS_ENDPOINT)
 		.then(res => res.data)
+		.catch(error => handleError(error));
+};
+
+const modifySettings = async (
+	client: HttpClientModule,
+	fn: (s: Settings) => void
+): Promise<void> => {
+	await getSettings(client)
+		.then(settings => client.put(SETTINGS_ENDPOINT, fn(settings)))
 		.catch(error => handleError(error));
 };
 
