1556e75b741b38b2f3b90d0e15c8890b206dd9a3
Jan Vogt <jvogt@sipgate.de>
2019-11-05 14:29:36 +0100

    feat: Add examples for settings module

--- examples/settings/settings.ts
+++ examples/settings/settings.ts
@@ -1,15 +1,24 @@
 import { createClient } from '../../lib/core/sipgateClient';
 
-const username = process.env.SIPGATE_USERNAME || '';
-const password = process.env.SIPGATE_PASSWORD || '';
+(async () => {
+	const username = process.env.SIPGATE_USERNAME || '';
+	const password = process.env.SIPGATE_PASSWORD || '';
 
-const client = createClient(username, password);
+	const client = createClient(username, password);
 
-client.settings
-	.setIncomingUrl('https://io.sipgate.beer/my/incoming/url')
-	.then(result => {
-		console.log('Incoming URL updated.', { result });
-	})
-	.catch(error => {
-		throw error;
-	});
+	await client.settings.setIncomingUrl(
+		'https://io.sipgate.beer/my/incoming/url2'
+	);
+	console.log('Incoming URL updated.');
+
+	await client.settings.setOutgoingUrl(
+		'https://io.sipgate.beer/my/outgoing/url2'
+	);
+	console.log('Outgoing URL updated.');
+
+	await client.settings.setLog(true);
+	console.log('Logging enabled.');
+
+	await client.settings.setWhitelist(['p2', 'g10']);
+	console.log('Whitelist updated.');
+})();
