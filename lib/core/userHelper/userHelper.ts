55c333512fae50163b04c49d82ca4485165a1152
Sven Kube <kube@sipgate.de>
2019-11-15 11:06:06 +0100

    refactor: simplify request handling in userHelper

--- lib/core/userHelper/userHelper.ts
+++ lib/core/userHelper/userHelper.ts
@@ -5,11 +5,8 @@ import handleCoreError from '../errors/handleCoreError';
 export const getUserInfo = async (
 	client: HttpClientModule
 ): Promise<UserInfo> => {
-	try {
-		const { data } = await client.get<UserInfo>('/authorization/userinfo');
-		return data;
-	} catch (e) {
-		const newError = handleCoreError(e);
-		return Promise.reject(newError);
-	}
+	return await client
+		.get<UserInfo>('/authorization/userinfo')
+		.then(res => res.data)
+		.catch(error => Promise.reject(handleCoreError(error)));
 };
