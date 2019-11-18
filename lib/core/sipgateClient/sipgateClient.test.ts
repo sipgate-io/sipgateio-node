7f7c0cafda9f2b1b64b06cdc38856d1f0bcfaf1f
Sven Kube <kube@sipgate.de>
2019-11-18 13:08:53 +0100

    test: remove useless test covered by typesystem

--- lib/core/sipgateClient/sipgateClient.test.ts
+++
@@ -1,8 +0,0 @@
-import { createClient } from './sipgateClient';
-
-describe('sipgateClient tests', () => {
-	test('check object', () => {
-		const client = createClient('test@test.com', 'test');
-		expect(client).toHaveProperty('sms');
-	});
-});
