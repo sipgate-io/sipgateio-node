1812441335abd1f62722aad4912917a088b8b10f
Sven Kube <kube@sipgate.de>
2019-11-18 15:17:05 +0100

    refactor: adjust example to refactored fax sending api

--- examples/fax/fax.ts
+++ examples/fax/fax.ts
@@ -2,22 +2,28 @@ import * as fs from 'fs';
 import * as path from 'path';
 import { createClient } from '../../lib/core/sipgateClient';
 
-const password = process.env.SIPGATE_PASSWORD || '';
-const username = process.env.SIPGATE_USERNAME || '';
+(async (): Promise<void> => {
+	const password = process.env.SIPGATE_PASSWORD || '';
+	const username = process.env.SIPGATE_USERNAME || '';
 
-const client = createClient(username, password);
+	const client = createClient(username, password);
 
-const faxlineId = process.env.SIPGATE_FAX_EXTENSION || '';
-const recipient = process.env.SIPGATE_FAX_RECIPIENT || '';
-const filePath = './testpage.pdf';
-const { name: filename } = path.parse(path.basename(filePath));
-const fileContent = fs.readFileSync(filePath);
+	const faxlineId = process.env.SIPGATE_FAX_EXTENSION || '';
+	const recipient = process.env.SIPGATE_FAX_RECIPIENT || '';
+	const filePath = './testpage.pdf';
+	const { name: filename } = path.parse(path.basename(filePath));
+	const fileContent = fs.readFileSync(filePath);
 
-client.fax
-	.send({ recipient, fileContent, filename, faxlineId })
-	.then(() => {
-		console.log('Fax sent');
-	})
-	.catch(error => {
-		console.error(error.message);
+	const sendFaxResponse = await client.fax.send({
+		recipient,
+		fileContent,
+		filename,
+		faxlineId,
 	});
+
+	console.log(`Fax sent with id: ${sendFaxResponse.sessionId}`);
+
+	const faxStatus = await client.fax.getFaxStatus(sendFaxResponse.sessionId);
+
+	console.log(`Fax status: ${faxStatus}`);
+})();
