35d719019e87c27f1dbe88c92597d66f0b608c81
Florian Brandt <fbrandt@sipgate.de>
2019-11-19 13:06:38 +0100

    feat: Phonenumber validation with google-libphonenumber (E.164 Format)
    Co-Authored-By: Sven Kube <kube@sipgate.de>
    Co-Authored-By: Jan Vogt <jvogt@sipgate.de>

--- lib/core/validator/validatePhoneNumber.ts
+++ lib/core/validator/validatePhoneNumber.ts
@@ -1,16 +1,19 @@
 import { ErrorMessage } from '../errors';
+import { PhoneNumberFormat, PhoneNumberUtil } from 'google-libphonenumber';
 import { ValidationResult } from './validationResult';
 
 const validatePhoneNumber = (phoneNumber: string): ValidationResult => {
-	const phoneNumberRegex = new RegExp(/^\+?[0-9]+$/);
+	const phoneNumberUtil = PhoneNumberUtil.getInstance();
+	try {
+		const parsedPhoneNumber = phoneNumberUtil.parse(phoneNumber);
+		phoneNumberUtil.format(parsedPhoneNumber, PhoneNumberFormat.E164);
 
-	if (!phoneNumberRegex.test(phoneNumber)) {
+		return { isValid: true };
+	} catch (exception) {
 		return {
 			cause: `${ErrorMessage.VALIDATOR_INVALID_PHONE_NUMBER}: ${phoneNumber}`,
 			isValid: false,
 		};
 	}
-
-	return { isValid: true };
 };
 export { validatePhoneNumber };
