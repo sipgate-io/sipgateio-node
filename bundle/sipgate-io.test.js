8bc5b5cffe930dcb3293ea041ad6b22bdbf91f7c
Florian Brandt <fbrandt@sipgate.de>
2019-11-19 11:12:27 +0100

    test: fix client creation in bundle
    Co-Authored-By: Sven Kube <kube@sipgate.de>
    Co-Authored-By: Jan Vogt <jvogt@sipgate.de>

--- bundle/sipgate-io.test.js
+++ bundle/sipgate-io.test.js
@@ -1,102 +1,102 @@
 const fs = require('fs');
 
 describe('sipgate-io module', () => {
-
 	let dom;
 	let bundle;
 
 	beforeAll(() => {
-		const {JSDOM} = require('jsdom');
-		let io = fs.readFileSync('./bundle/sipgate-io.js');
-		let html = `<!DOCTYPE html><script>${io}</script>`;
-		dom = new JSDOM(html, {runScripts: 'dangerously'});
+		const { JSDOM } = require('jsdom');
+		const io = fs.readFileSync('./bundle/sipgate-io.js');
+		const html = `<!DOCTYPE html><script>${io}</script>`;
+		dom = new JSDOM(html, { runScripts: 'dangerously' });
 		bundle = dom.window.require('sipgate-io');
 	});
 
 	it('should have a createClient method', () => {
 		expect(bundle.createClient).toBeDefined();
 	});
-
 });
 
 describe('sipgate-io client', () => {
-
 	let dom;
 	let bundle;
 	let sipgateClient;
 
 	beforeAll(() => {
-		const {JSDOM} = require('jsdom');
-		let io = fs.readFileSync('./bundle/sipgate-io.js');
-		let html = `<!DOCTYPE html><script>${io}</script>`;
-		dom = new JSDOM(html, {runScripts: 'dangerously'});
+		const { JSDOM } = require('jsdom');
+		const io = fs.readFileSync('./bundle/sipgate-io.js');
+		const html = `<!DOCTYPE html><script>${io}</script>`;
+		dom = new JSDOM(html, { runScripts: 'dangerously' });
 		bundle = dom.window.require('sipgate-io');
-		sipgateClient = bundle.createClient('dummy@sipgate.de', '1234');
+		sipgateClient = bundle.createClient({
+			username: 'dummy@sipgate.de',
+			password: '1234',
+		});
 	});
 
 	it('should contain a call module with an initiate function', () => {
 		expect(sipgateClient.call).toBeDefined();
 		expect(sipgateClient.call.initiate).toBeDefined();
-		expect(typeof (sipgateClient.call.initiate)).toEqual('function');
+		expect(typeof sipgateClient.call.initiate).toEqual('function');
 	});
 
 	it('should contain a fax module with a send function', () => {
 		expect(sipgateClient.fax).toBeDefined();
 		expect(sipgateClient.fax.send).toBeDefined();
-		expect(typeof (sipgateClient.fax.send)).toEqual('function');
+		expect(typeof sipgateClient.fax.send).toEqual('function');
 	});
 
 	it('should contain a sms module with a send function', () => {
 		expect(sipgateClient.sms).toBeDefined();
 		expect(sipgateClient.sms.send).toBeDefined();
-		expect(typeof (sipgateClient.sms.send)).toEqual('function');
+		expect(typeof sipgateClient.sms.send).toEqual('function');
 	});
 
 	it('should contain a settings module with a setIncomingUrl function', () => {
 		expect(sipgateClient.settings).toBeDefined();
 		expect(sipgateClient.settings.setIncomingUrl).toBeDefined();
-		expect(typeof (sipgateClient.settings.setIncomingUrl)).toEqual('function');
+		expect(typeof sipgateClient.settings.setIncomingUrl).toEqual('function');
 	});
 
 	it('should contain a settings module with a setOutgoingUrl function', () => {
 		expect(sipgateClient.settings).toBeDefined();
 		expect(sipgateClient.settings.setOutgoingUrl).toBeDefined();
-		expect(typeof (sipgateClient.settings.setOutgoingUrl)).toEqual('function');
+		expect(typeof sipgateClient.settings.setOutgoingUrl).toEqual('function');
 	});
 
 	it('should contain a settings module with a setWhitelist function', () => {
 		expect(sipgateClient.settings).toBeDefined();
 		expect(sipgateClient.settings.setWhitelist).toBeDefined();
-		expect(typeof (sipgateClient.settings.setWhitelist)).toEqual('function');
+		expect(typeof sipgateClient.settings.setWhitelist).toEqual('function');
 	});
 
 	it('should contain a settings module with a setLog function', () => {
 		expect(sipgateClient.settings).toBeDefined();
 		expect(sipgateClient.settings.setLog).toBeDefined();
-		expect(typeof (sipgateClient.settings.setLog)).toEqual('function');
+		expect(typeof sipgateClient.settings.setLog).toEqual('function');
 	});
 
 	it('should contain a settings module with a clearIncomingUrl function', () => {
 		expect(sipgateClient.settings).toBeDefined();
 		expect(sipgateClient.settings.clearIncomingUrl).toBeDefined();
-		expect(typeof (sipgateClient.settings.clearIncomingUrl)).toEqual('function');
+		expect(typeof sipgateClient.settings.clearIncomingUrl).toEqual('function');
 	});
 
 	it('should contain a settings module with a clearOutgoingUrl function', () => {
 		expect(sipgateClient.settings).toBeDefined();
 		expect(sipgateClient.settings.clearOutgoingUrl).toBeDefined();
-		expect(typeof (sipgateClient.settings.clearOutgoingUrl)).toEqual('function');
+		expect(typeof sipgateClient.settings.clearOutgoingUrl).toEqual('function');
 	});
 
 	it('should contain a settings module with a clearWhitelist function', () => {
 		expect(sipgateClient.settings).toBeDefined();
 		expect(sipgateClient.settings.clearWhitelist).toBeDefined();
-		expect(typeof (sipgateClient.settings.clearWhitelist)).toEqual('function');
+		expect(typeof sipgateClient.settings.clearWhitelist).toEqual('function');
 	});
 
 	it('should contain a settings module with a disableWhitelist function', () => {
 		expect(sipgateClient.settings).toBeDefined();
 		expect(sipgateClient.settings.disableWhitelist).toBeDefined();
-		expect(typeof (sipgateClient.settings.disableWhitelist)).toEqual('function');
+		expect(typeof sipgateClient.settings.disableWhitelist).toEqual('function');
 	});
 });
