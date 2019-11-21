675838461b4e13dbe3b4ef31a8a1e96eb53388b6
Maximilian Massing <massing@sipgate.de>
2019-11-21 16:41:48 +0100

    test: add test for empty lines

--- lib/contacts/contacts.test.ts
+++ lib/contacts/contacts.test.ts
@@ -26,20 +26,21 @@ describe('Contacts Module', () => {
 	});
 
 	it.each`
-		input                                                                   | expected
-		${'firstname,lastname,number\nAlan,Turing,+4921163553355'}              | ${'firstname,lastname,number\nAlan,Turing,+4921163553355'}
-		${'lastname,firstname,number\nTuring,Alan,+4921163553355'}              | ${'firstname,lastname,number\nAlan,Turing,+4921163553355'}
-		${'number,firstname,lastname\n+4921163553355,Alan,Turing'}              | ${'firstname,lastname,number\nAlan,Turing,+4921163553355'}
-		${'firstname,number,lastname\nAlan,+4921163553355,Turing'}              | ${'firstname,lastname,number\nAlan,Turing,+4921163553355'}
-		${'lastname,number,firstname\nTuring,+4921163553355,Alan'}              | ${'firstname,lastname,number\nAlan,Turing,+4921163553355'}
-		${'number,lastname,firstname\n+4921163553355,Turing,Alan'}              | ${'firstname,lastname,number\nAlan,Turing,+4921163553355'}
-		${'Firstname,Lastname,Number\nAlan,Turing,+4921163553355'}              | ${'firstname,lastname,number\nAlan,Turing,+4921163553355'}
-		${'alias,number,lastname,firstname\nEnigma,+4921163553355,Turing,Alan'} | ${'firstname,lastname,number\nAlan,Turing,+4921163553355'}
-		${'number,lastname,alias,firstname\n+4921163553355,Turing,Enigma,Alan'} | ${'firstname,lastname,number\nAlan,Turing,+4921163553355'}
-		${'number,lastname,firstname,alias\n+4921163553355,Turing,Alan,Enigma'} | ${'firstname,lastname,number\nAlan,Turing,+4921163553355'}
-		${'alias,number,lastname,firstname\n,+4921163553355,Turing,Alan'}       | ${'firstname,lastname,number\nAlan,Turing,+4921163553355'}
-		${'number,lastname,alias,firstname\n+4921163553355,Turing,,Alan'}       | ${'firstname,lastname,number\nAlan,Turing,+4921163553355'}
-		${'number,lastname,firstname,alias\n+4921163553355,Turing,Alan,'}       | ${'firstname,lastname,number\nAlan,Turing,+4921163553355'}
+		input                                                                                               | expected
+		${'firstname,lastname,number\nAlan,Turing,+4921163553355'}                                          | ${'firstname,lastname,number\nAlan,Turing,+4921163553355'}
+		${'lastname,firstname,number\nTuring,Alan,+4921163553355'}                                          | ${'firstname,lastname,number\nAlan,Turing,+4921163553355'}
+		${'number,firstname,lastname\n+4921163553355,Alan,Turing'}                                          | ${'firstname,lastname,number\nAlan,Turing,+4921163553355'}
+		${'firstname,number,lastname\nAlan,+4921163553355,Turing'}                                          | ${'firstname,lastname,number\nAlan,Turing,+4921163553355'}
+		${'lastname,number,firstname\nTuring,+4921163553355,Alan'}                                          | ${'firstname,lastname,number\nAlan,Turing,+4921163553355'}
+		${'number,lastname,firstname\n+4921163553355,Turing,Alan'}                                          | ${'firstname,lastname,number\nAlan,Turing,+4921163553355'}
+		${'Firstname,Lastname,Number\nAlan,Turing,+4921163553355'}                                          | ${'firstname,lastname,number\nAlan,Turing,+4921163553355'}
+		${'alias,number,lastname,firstname\nEnigma,+4921163553355,Turing,Alan'}                             | ${'firstname,lastname,number\nAlan,Turing,+4921163553355'}
+		${'number,lastname,alias,firstname\n+4921163553355,Turing,Enigma,Alan'}                             | ${'firstname,lastname,number\nAlan,Turing,+4921163553355'}
+		${'number,lastname,firstname,alias\n+4921163553355,Turing,Alan,Enigma'}                             | ${'firstname,lastname,number\nAlan,Turing,+4921163553355'}
+		${'alias,number,lastname,firstname\n,+4921163553355,Turing,Alan'}                                   | ${'firstname,lastname,number\nAlan,Turing,+4921163553355'}
+		${'number,lastname,alias,firstname\n+4921163553355,Turing,,Alan'}                                   | ${'firstname,lastname,number\nAlan,Turing,+4921163553355'}
+		${'number,lastname,firstname,alias\n+4921163553355,Turing,Alan,'}                                   | ${'firstname,lastname,number\nAlan,Turing,+4921163553355'}
+		${'\nnumber,lastname,firstname,alias\n\n+4921163553355,Turing,Alan,\n+4921163553355,Lovelace,Ada,'} | ${'firstname,lastname,number\nAlan,Turing,+4921163553355\nAda,Lovelace,+4921163553355'}
 	`('should parse csv successfully', async ({ input, expected }) => {
 		await contactsModule.importFromCsvString(input);
 		expect(csvContent).toEqual(expected);
@@ -68,7 +69,6 @@ describe('Contacts Module', () => {
 		${'firstname,lastname,number\nm,turing,000\na,000\na,b,000'} | ${ErrorMessage.CONTACTS_MISSING_VALUES}
 		${'firstname,lastname,number\nm,turing,000\na,000,200\na,b'} | ${ErrorMessage.CONTACTS_MISSING_VALUES}
 		${'firstname,lastname,number\nturing,000\na,000,c\na,b,000'} | ${ErrorMessage.CONTACTS_MISSING_VALUES}
-		${'firstname,lastname,number\n,,\n\n'}                       | ${ErrorMessage.CONTACTS_MISSING_VALUES}
 	`(
 		'throws $expected when $input is given (some values are missing)',
 		({ input, expected }) => {
