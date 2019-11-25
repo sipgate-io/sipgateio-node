9a6430897e8389c3ab1bfa81a96c5a68e09259a4
Jan Schmitt <schmitt@sipgate.de>
2019-11-25 17:11:06 +0100

    refactor: import OAuthToken constant and remove local constant

--- lib/core/validator/validateOAuthToken.test.ts
+++ lib/core/validator/validateOAuthToken.test.ts
@@ -1,10 +1,8 @@
 import { ErrorMessage } from '../errors';
 import { validateOAuthToken } from './validateOAuthToken';
+import validOAuthToken from './validOAuthToken';
 
 describe('ValidateOAuthToken', () => {
-	const validOAuthToken =
-		'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJqdGkiOiJmM2U3ZzI0NC0yNDQ3LTQ1ODctOWZjYy05ZWY1MjQ3aDE3NHMiLCJleHAiOjE1NDY2NTQ4MjcsIm5iZiI6MCwiaWF0IjoxNTY1NDgzMjE4LCJpc3MiOiJodHRwczovL2xvZ2luLnNpcGdhdGUuY29tL2F1dGgvcmVhbG1zL3RoaXJkLXBhcnR5Iiwic3ViIjoiZjoyZTc0ODY1Ny1mNTV6LTg5Z3MtOWdmMi1ydDU4MjRoMjQ1MTg6ODQ1Mjg0NiIsInR5cCI6IkJlYXJlciIsImF6cCI6InNpcGdhdGUtc3dhZ2dlci11aSIsIm5vbmNlIjoiOTgyMTU3MSIsImF1dGhfdGltZSI6MTU2NTQyODU0OCwic2Vzc2lvbl9zdGF0ZSI6Ijg1ZzR6MXM3LTc4ZzItNDM4NS05ZTFnLXIxODdmMjc0ZWQ5ayIsImFjciI6IjAiLCJzY29wZSI6ImFsbCJ9.axEQX90FLk4W89y92C9eQnwMV3wfewk5zaPCszj46YA';
-
 	test.each`
 		input                                         | expected
 		${validOAuthToken}                            | ${{ isValid: true }}
