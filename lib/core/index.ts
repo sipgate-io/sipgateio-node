9c5d042d3b4ed31ba5e13d4fdb0580509785612a
Florian Brandt <fbrandt@sipgate.de>
2019-11-26 13:16:55 +0100

    fix: remove missing module from export
    Co-Authored-By: Jan Schmitt <schmitt@sipgate.de>

--- lib/core/index.ts
+++ lib/core/index.ts
@@ -2,5 +2,4 @@ export * from './errors';
 export * from './httpClient/httpClient';
 export * from './models';
 export * from './sipgateIOClient';
-export * from './userHelper';
 export * from './validator';
