dc4edecb2732b2d71ba15dec94c6af3470504b3e
Florian Brandt <fbrandt@sipgate.de>
2019-11-26 10:19:53 +0100

    refactor: change Order of send-At Validation checks
    Co-Authored-By: Jan Schmitt <schmitt@sipgate.de>

--- lib/core/validator/validateSendAt.ts
+++ lib/core/validator/validateSendAt.ts
@@ -2,6 +2,13 @@ import { ErrorMessage } from '../errors';
 import { ValidationResult } from './validationResult';
 
 const validateSendAt = (sendAt: Date): ValidationResult => {
+	if (Number.isNaN(sendAt.getTime())) {
+		return {
+			cause: ErrorMessage.SMS_TIME_INVALID,
+			isValid: false,
+		};
+	}
+
 	if (sendAt.getTime() < Date.now()) {
 		return {
 			cause: ErrorMessage.SMS_TIME_MUST_BE_IN_FUTURE,
@@ -12,13 +19,6 @@ const validateSendAt = (sendAt: Date): ValidationResult => {
 	if (sendAt.getTime() > Date.now() + 30 * 24 * 60 * 60 * 1000) {
 		return {
 			cause: ErrorMessage.SMS_TIME_TOO_FAR_IN_FUTURE,
-			isValid: false,
-		};
-	}
-
-	if (Number.isNaN(sendAt.getTime())) {
-		return {
-			cause: ErrorMessage.SMS_TIME_INVALID,
 			isValid: false,
 		};
 	}
