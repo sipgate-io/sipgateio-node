16a745e549da7f96f413e76305f08b6db7fca5d9
Florian Brandt <fbrandt@sipgate.de>
2019-12-02 11:30:21 +0100

    fix: remove new line in update_version.sh
    Co-Authored-By: Sven Kube <kube@sipgate.de>

--- update_version.sh
+++ update_version.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env sh
 
-grep '"version":' package.json | sed -e 's/\(.*\),/{\n\1\n}/' > lib/version.json
+grep '"version":' package.json | sed -e 's/\(.*\),/{\1}/' > lib/version.json
 git add lib/version.json
