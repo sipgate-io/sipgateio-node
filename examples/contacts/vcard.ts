001967b48168e4ffc8672c5169c43642a9452012
Florian Brandt <fbrandt@sipgate.de>
2019-12-19 09:01:54 +0100

    fix: add missing file for example

---
+++ examples/contacts/vcard.ts
@@ -0,0 +1,21 @@
+export const vcardExample =
+	'BEGIN:VCARD\r\n' +
+	'VERSION:4.0\r\n' +
+	'N:Doe;John;Mr.;\r\n' +
+	'FN:John Doe\r\n' +
+	'ORG:Example.com Inc.\r\n' +
+	'TITLE:Imaginary test person\r\n' +
+	'EMAIL;type=INTERNET;type=WORK;type=pref:johnDoe@example.org\r\n' +
+	'TEL;type=HOME:+4915199999999\r\n' +
+	'item1.ADR;type=WORK:postbox;extended adress;2 Enterprise Avenue;Worktown;NY;01111;USA\r\n' +
+	'item1.X-ABADR:us\r\n' +
+	'NOTE:John Doe has a long and varied history\\, being documented on more police files that anyone else. Reports of his death are alas numerous.\r\n' +
+	'item3.URL;type=pref:http\\://www.example/com/doe\r\n' +
+	'item3.X-ABLabel:_$!<HomePage>!$_\r\n' +
+	'item4.URL:http\\://www.example.com/Joe/foaf.df\r\n' +
+	'item4.X-ABLabel:FOAF\r\n' +
+	'item5.X-ABRELATEDNAMES;type=pref:Jane Doe\r\n' +
+	'item5.X-ABLabel:_$!<Friend>!$_\r\n' +
+	'CATEGORIES:Work,Test group\r\n' +
+	'X-ABUID:5AD380FD-B2DE-4261-BA99-DE1D1DB52FBE\\:ABPerson\r\n' +
+	'END:VCARD\r\n';
