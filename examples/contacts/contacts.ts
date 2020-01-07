3579f91e685d9a5fde01f191a34484ac232125b9
Florian Brandt <fbrandt@sipgate.de>
2020-01-07 13:38:48 +0100

    feat: add examples for single contact import
    Co-Authored-By: Jan Vogt <jvogt@sipgate.de>
    Co-Authored-By: Jan Schmitt <schmitt@sipgate.de>

--- examples/contacts/contacts.ts
+++ examples/contacts/contacts.ts
@@ -22,6 +22,50 @@ client.contacts
 		console.error(error.message);
 	});
 
-client.contacts.importVCardString(vcardExample, 'PRIVATE');
+client.contacts
+	.import(
+		{
+			firstname: 'John',
+			lastname: 'Doe',
+			phone: { number: '+49123456789', type: ['HOME'] },
+			address: {
+				country: 'Germany',
+				extendedAddress: 'Extended Address',
+				locality: 'DE',
+				poBox: 'Post Box',
+				postalCode: '0000',
+				region: 'region',
+				streetAddress: 'street',
+			},
+			email: {
+				email: 'John.Doe@example.org',
+				type: ['HOME'],
+			},
+			organization: ['example', 'org'],
+		},
+		'PRIVATE'
+	)
+	.then(() => {
+		console.log('Contact imported successfully');
+	})
+	.catch(error => {
+		console.error(error.message);
+	});
 
-client.contacts.importVCardString(vcardExample, 'SHARED');
+client.contacts
+	.importVCardString(vcardExample, 'PRIVATE')
+	.then(() => {
+		console.log('Contact imported successfully');
+	})
+	.catch(error => {
+		console.error(error.message);
+	});
+
+client.contacts
+	.importVCardString(vcardExample, 'SHARED')
+	.then(() => {
+		console.log('Contact imported successfully');
+	})
+	.catch(error => {
+		console.error(error.message);
+	});
