611aeba7e94637c3d1bd22fc69c54f23678e4cd8
Jan Schmitt <schmitt@sipgate.de>
2020-02-17 17:38:17 +0100

    test: add test for already used port
    Co-Authored-By: Sven Kube <kube@sipgate.de>

--- lib/webhook/webhook.test.ts
+++ lib/webhook/webhook.test.ts
@@ -1,19 +1,28 @@
 import { WebhookModule } from './webhook.module';
 import { createWebhookModule } from './webhook';
 
-describe('get settings', () => {
+describe('create webhook module', () => {
 	let webhookModule: WebhookModule;
 
 	beforeAll(() => {
 		webhookModule = createWebhookModule();
 	});
 
-	it('should use the endpoint "settings/sipgateio"', async () => {
+	afterEach(() => {
+		if (webhookModule.server !== undefined) {
+			webhookModule.server.close();
+		}
+	});
+
+	it('should create server', () => {
 		webhookModule.createServer(1234);
 		const { server } = webhookModule;
 		expect(server).toBeDefined();
-		if (server !== undefined) {
-			server.close();
-		}
+	});
+
+	it('expect exception when starting on same port', async () => {
+		const webhookModuleTwo = createWebhookModule();
+		await webhookModule.createServer(1234);
+		await expect(webhookModuleTwo.createServer(1234)).rejects.toThrow();
 	});
 });
