bf5043ba3433410272540de67ab098316ddee0d5
Jan Schmitt <schmitt@sipgate.de>
2020-02-17 17:32:42 +0100

    chore: remove run.ts
    Co-Authored-By: Sven Kube <kube@sipgate.de>

--- run.ts
+++
@@ -1,9 +0,0 @@
-import { EventType, createWebhookModule } from './lib/webhook';
-
-const webhookModule = createWebhookModule();
-
-const webhooks = webhookModule.createServer(1234);
-
-webhooks.on(EventType.ON_ANSWER, () => {
-	return '<bla></bla>';
-});
