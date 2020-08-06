6d3ccbc34e6e65f03a021d24076b483f7777702f
Björn Vopel <vopel@sipgate.de>
2020-08-06 16:05:40 +0200

    refactor: rename webhookSettingsError enum
    Co-Authored-By: David Borzek <borzek@sipgate.de>

--- lib/webhook-settings/errors/handleWebhookSettingError.ts
+++ lib/webhook-settings/errors/handleWebhookSettingError.ts
@@ -1,13 +1,13 @@
 import { HttpError, handleCoreError } from '../../core';
 
-export enum WebhookSettingErrorMessage {
+export enum WebhookSettingsErrorMessage {
 	WEBHOOK_SETTINGS_FEATURE_NOT_BOOKED = 'sipgateIO is not booked for your account',
 }
 
 export const handleWebhookSettingsError = (error: HttpError): Error => {
 	if (error.response && error.response.status === 403) {
 		return new Error(
-			WebhookSettingErrorMessage.WEBHOOK_SETTINGS_FEATURE_NOT_BOOKED
+			WebhookSettingsErrorMessage.WEBHOOK_SETTINGS_FEATURE_NOT_BOOKED
 		);
 	}
 	return handleCoreError(error);
