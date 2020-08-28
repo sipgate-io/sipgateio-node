38fdcd2421e0b4a148140658f0a9e6bfb1e6a6af
Jakob Hellermann <jakob.hellermann@protonmail.com>
2020-08-28 10:54:33 +0200

    refactor: change attribution to link github repository directly
    Co-authored-by: Paul Schwörer <schwoerer@sipgate.de>
    Co-authored-by: Robert Deppe <deppe@sipgate.de>

--- lib/fax/validators/validatePdfFileContent.ts
+++ lib/fax/validators/validatePdfFileContent.ts
@@ -4,7 +4,7 @@ export enum FaxValidationMessage {
 	INVALID_PDF_MIME_TYPE = 'Invalid PDF file',
 }
 
-// see https://github.com/MaraniMatias/isPDF/blob/946ffba1ccbca4d7b88bda39e2dda426bd5b0977/index.js#L5
+// taken from https://github.com/MaraniMatias/isPDF
 const isValidPDF = (buffer: Buffer): boolean => {
 	return buffer.lastIndexOf('%PDF-') === 0 && buffer.lastIndexOf('%%EOF') > -1;
 };
