51c15c30cd02f5b7ead3020ca9be146c01c1aecf
Jakob Hellermann <jakob.hellermann@protonmail.com>
2021-01-15 14:27:14 +0100

    chore: increase library version to v2.1.5
    Co-authored-by: David Borzek <borzek@sipgate.de>

--- update_version.sh
+++ update_version.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env sh
 
 grep '"version":' package.json | sed -e 's/\(.*\),/{\1}/' > lib/version.json
-prettier --write lib/version.json
+./node_modules/.bin/prettier --write lib/version.json
 git add lib/version.json
