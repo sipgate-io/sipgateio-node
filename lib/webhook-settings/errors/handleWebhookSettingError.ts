3100a318f3fc05bf82fa76dae86af907f4261abe
Daniel Knapp <knapp@sipgate.de>
2021-06-16 08:48:53 +0200

    fix: Add additional description to WEBHOOK_SETTINGS_FEATURE_NOT_BOOKED
    Co-authored-by: David Borzek <borzek@sipgate.de>
    Co-authored-by: Florian Mai <mai@sipgate.de>
    Co-authored-by: James Tophoven <tophoven@sipgate.de>
    Co-authored-by: Manuel Arnold <arnold@sipgate.de>
    Co-authored-by: Marco Laubinger <laubinger@sipgate.de>
    Co-authored-by: Michel Schwarz <mschwarz@sipgate.de>

--- lib/webhook-settings/errors/handleWebhookSettingError.ts
+++ lib/webhook-settings/errors/handleWebhookSettingError.ts
@@ -1,7 +1,7 @@
 import { HttpError, handleCoreError } from '../../core';
 
 export enum WebhookSettingsErrorMessage {
-	WEBHOOK_SETTINGS_FEATURE_NOT_BOOKED = 'sipgateIO is not booked for your account',
+	WEBHOOK_SETTINGS_FEATURE_NOT_BOOKED = 'sipgateIO is not booked for your account (or insufficient scopes)',
 }
 
 export const handleWebhookSettingsError = (
